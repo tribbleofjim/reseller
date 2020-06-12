8f78409200c197836a9b4bc662ac258fa7b6508e
GAI0603 <49257707+GAI0603@users.noreply.github.com>
2020-06-13 00:20:38 +0800

    注册测试

--- src/main/java/com/web/reseller/dao/L2UserMapper.java
+++ src/main/java/com/web/reseller/dao/L2UserMapper.java
@@ -1,7 +1,10 @@
 package com.web.reseller.controller.dao;
 
 import com.web.reseller.model.L2User;
+import org.apache.ibatis.annotations.Param;
 
 public interface L2UserMapper {
     L2User login(String loginName);
+
+void register(L2User l2User);
 }
