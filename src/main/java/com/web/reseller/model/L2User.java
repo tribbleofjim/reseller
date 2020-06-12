654d6c5c3540dfb02aa1d2c64fc40326bc3af68a
GAI0603 <49257707+GAI0603@users.noreply.github.com>
2020-06-13 00:21:26 +0800

    注册测试

--- src/main/java/com/web/reseller/model/L2User.java
+++ src/main/java/com/web/reseller/model/L2User.java
@@ -5,6 +5,47 @@ public class L2User {
 
     private String password;
 
+    private String phonenumber;
+
+    private int age;
+    private String qqnumber;
+    private String city;
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getQqnumber() {
+        return qqnumber;
+    }
+
+    public void setQqnumber(String qqnumber) {
+        this.qqnumber = qqnumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+
+
     public String getLoginname() {
         return loginname;
     }
