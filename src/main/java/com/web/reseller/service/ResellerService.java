c543f282d66c11f82a6cfdfa6c80a1f64c3ee943
a1506733879 <66730671+a1506733879@users.noreply.github.com>
2020-06-26 00:41:56 +0800

    增加查看订单与提成

--- src/main/java/com/web/reseller/service/ResellerService.java
+++ src/main/java/com/web/reseller/service/ResellerService.java
@@ -15,6 +15,8 @@ public class ResellerService {
     private L2TourMapper l2TourMapper;
     @Autowired
     private L2UserMapper l2UserMapper;
+    private L2OrderMapper l2OrderMapper;
+    
     public result productsList(String phoneNumber){
         List<L2Tour> list =l2TourMapper.getProductList(phoneNumber);
         if (list.size()==0)
@@ -30,7 +32,23 @@ public class ResellerService {
         result getResult=new result(200,list.size(),"success",list);
         return getResult;
     }
-
+    
+    public result orderList(String phonenumber){
+        List<L2Order>list =l2OrderMapper.resellersList(phonenumber);
+        if (list.size()==0)
+            return result.No_order;
+        result getResult=new result(200,list.size(),"success",list);
+        return getResult;
+    }
+    
+    public result percentage(String phonenumber){
+        List<L2User>list =l2UserMapper.resellersList(phonenumber);
+        if (list.size()==0)
+            return result.No_percentage;
+        result getResult=new result(200,list.size(),"success",list);
+        return getResult;
+    }
+    
     public result getSuperior(String phoneNumber){
         return new result(200, 0, "success", l2UserMapper.getSuperior(phoneNumber));
     }
