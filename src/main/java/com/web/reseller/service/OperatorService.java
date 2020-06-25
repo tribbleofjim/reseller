d3e1f4a8f9a1a0288c5c2d249562ed4a42611852
a1506733879 <66730671+a1506733879@users.noreply.github.com>
2020-06-26 00:38:55 +0800

    分销商业务员 查看总提成

---
+++ src/main/java/com/web/reseller/service/OperatorService.java
@@ -0,0 +1,30 @@
+package com.web.reseller.service;
+
+import com.web.reseller.dao.L2TourMapper;
+import com.web.reseller.dao.L2UserMapper;
+import com.web.reseller.dao.L2role2productMapper;
+import com.web.reseller.model.L2Role2product;
+import com.web.reseller.model.L2Tour;
+import com.web.reseller.model.L2User;
+import com.web.reseller.util.result;
+import com.web.reseller.util.message;
+import com.web.reseller.util.result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+@Service
+public class OperatorService {
+	private L2OrderMapper l2OrderMapper;
+	
+	public result percentage(String phonenumber){
+        List<L2User>list =l2UserMapper.resellersList(phonenumber);
+        if (list.size()==0)
+            return result.No_reseller;
+        result getResult=new result(200,list.size(),"success",list);
+        return getResult;
+    }
+}
