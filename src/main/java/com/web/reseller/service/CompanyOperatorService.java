765e75d01c1b6b49fcd7bc76c5484c3b8e6d2d73
a1506733879 <66730671+a1506733879@users.noreply.github.com>
2020-06-26 00:36:48 +0800

    增加查看订单

--- src/main/java/com/web/reseller/service/CompanyOperatorService.java
+++ src/main/java/com/web/reseller/service/CompanyOperatorService.java
@@ -22,7 +22,7 @@ public class CompanyOperatorService {
     private L2TourMapper l2TourMapper;
     @Autowired
     private L2UserMapper l2UserMapper;
-
+    private L2OrderMapper l2OrderMapper;
     @Autowired
     private L2role2productMapper l2role2productMapper;
     public result productsList(){
@@ -40,6 +40,15 @@ public class CompanyOperatorService {
         result getResult=new result(200,list.size(),"success",list);
         return getResult;
     }
+    
+    public result orderList(String phonenumber){
+        List<L2Order>list =l2OrderMapper.resellersList(phonenumber);
+        if (list.size()==0)
+            return result.No_reseller;
+        result getResult=new result(200,list.size(),"success",list);
+        return getResult;
+    }
+    
     public message distribute2reseller(String phonenumber,String productID){
         if(phonenumber==null||productID==null)
             return message.No_phone_productID;
